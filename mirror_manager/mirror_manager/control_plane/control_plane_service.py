92b011ac90c3bd200e6d4e5c0fea77adf88f7a6f
proman3419 <proman7268@gmail.com>
2025-05-17 18:13:38 +0200

    Update

--- mirror_manager/mirror_manager/control_plane/control_plane_service.py
+++ mirror_manager/mirror_manager/control_plane/control_plane_service.py
@@ -3,8 +3,6 @@ from kubernetes import config, client
 from kubernetes.config.config_exception import ConfigException
 from kubernetes.dynamic import DynamicClient
 
-from mirror_manager.api.response_models import ApplyOnControlPlaneRS
-
 
 class ControlPlaneService:
     def __init__(self):
@@ -14,13 +12,20 @@ class ControlPlaneService:
             config.load_kube_config()
         self.k8s_client = DynamicClient(client.ApiClient())
 
-    def apply(self, entity_definition: dict, change_id: int) -> ApplyOnControlPlaneRS:
+    def apply(self, entity_definition: dict, change_id: int) -> dict:
         try:
             api_version = entity_definition.get('apiVersion')
             kind = entity_definition.get('kind')
+            name = entity_definition.get('metadata', {}).get('name')
             dynamic_resource = self.k8s_client.resources.get(api_version=api_version, kind=kind)
-            obj = dynamic_resource.create(body=entity_definition)
-            return {"message": f"{kind} {obj.metadata.name} applied successfully", "stdout": str(obj)}
+            existing_obj = dynamic_resource.get(name=name)
+            if existing_obj:
+                updated_obj = dynamic_resource.patch(name=name, body=entity_definition,
+                                                     content_type='application/merge-patch+json')
+                return {"message": f"{kind} {name} updated successfully", "stdout": str(updated_obj)}
+            else:
+                obj = dynamic_resource.create(body=entity_definition)
+                return {"message": f"{kind} {name} created successfully", "stdout": str(obj)}
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
 
